afef2bd64665002568b438abee62d0568938431f
saianandmohan-cts <SaiAnand.Mohan@cognizant.com>
2026-05-14 10:38:51 +0530

    sai first commit

--- server/src/routes/patientRoutes.js
+++ server/src/routes/patientRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getPatientDashboard, updatePatient} = require('../controllers/patientController');
+const { getPatientDashboard, updatePatient, downloadPrescriptionData, viewPrescription} = require('../controllers/patientController');
 const { bookAppointment, updateAppointment } = require('../controllers/appointmentController');
 
 
@@ -9,6 +9,7 @@ router.get('/dashboard/:patientId',getPatientDashboard)
 router.patch('/updatePatient/:patientId',updatePatient)
 router.post('/book-appointment',bookAppointment);
 router.patch('/modify-appointment/:appointmentId',updateAppointment)
-
+router.get('/download-prescription/:consultationId',downloadPrescriptionData)
+router.get('/view-prescription:consultationId',viewPrescription)
 
 module.exports=router;
